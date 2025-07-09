5cebc84bb502817ba0b3680333fc007981fbbfa9
Zee <zack88340@gmail.com>
2025-07-09 21:54:50 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TravaCebu',
+  webDir: 'www'
+};
+
+export default config;

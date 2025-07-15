7aa303874bac8dcaa01d07b25c4201f2a1905088
Aaron <ajlayam2m@gmail.com>
2025-07-16 03:11:30 +0800

    added design to login, 5 taps admin developer mode

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +13,21 @@ export class LoginPage {
   email: string = '';
   password: string = '';
 
+  private headerTapCount = 0;
+  private tapTimeout: any;
+
   constructor(
     private authService: AuthService,
     private alertCtrl: AlertController,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private router: Router
   ) {}
+
+  
 async loginUser() {
   try {
     const userCredential = await this.authService.loginUser(this.email, this.password);
-    const uid = await this.authService.getCurrentUid(); // Or get directly from userCredential.user?.uid
+    const uid = await this.authService.getCurrentUid(); 
 
     if (uid) {
       this.navCtrl.navigateRoot(`/user-dashboard/${uid}`);
@@ -36,5 +43,19 @@ async loginUser() {
     await alert.present();
   }
 }
+
+tapHeader() {
+    this.headerTapCount++;
+
+    clearTimeout(this.tapTimeout);
+    this.tapTimeout = setTimeout(() => {
+      this.headerTapCount = 0;
+    }, 1000);
+
+    if (this.headerTapCount === 5) {
+      this.headerTapCount = 0;
+      this.router.navigate(['/admin/login']);
+    }
+  }
 
 }

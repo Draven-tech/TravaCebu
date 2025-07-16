2abfcfcbe857b3bd620f58e6abd97bd90a529d72
Zee <zack88340@gmail.com>
2025-07-16 19:35:08 +0800

    added welcome page and styling for admin-side features

---
+++ src/app/welcome/welcome.page.ts
@@ -0,0 +1,22 @@
+// welcome.page.ts
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  standalone: false,
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+})
+export class WelcomePage {
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+}

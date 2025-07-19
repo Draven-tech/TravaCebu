44cc12b8010c1754c26bff05d8ca99230f58980b
Zee <zack88340@gmail.com>
2025-07-19 19:14:59 +0800

    cleaned up itinerary modal

--- src/app/bucket-list/bucket-list.module.ts
+++ src/app/bucket-list/bucket-list.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { BucketListPageRoutingModule } from './bucket-list-routing.module';
 
 import { BucketListPage } from './bucket-list.page';
+import { ItineraryModalComponent } from './itinerary-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { BucketListPage } from './bucket-list.page';
     IonicModule,
     BucketListPageRoutingModule
   ],
-  declarations: [BucketListPage]
+  declarations: [BucketListPage, ItineraryModalComponent]
 })
 export class BucketListPageModule {}
